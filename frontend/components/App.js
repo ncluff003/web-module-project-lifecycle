2e2f52b54ad57b7d38864ec3d05434bd05fb5352
ncluff003 <ncluff003@zohomail.com>
2023-01-30 23:34:24 -0700

    Todos are both rendered, and now added.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,72 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import Form from "./Form";
 
-const URL = 'http://localhost:9000/api/todos'
+const URL = "http://localhost:9000/api/todos";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      todo: {
+        name: "",
+      },
+    };
+  }
+
+  componentDidMount() {
+    const getToDos = async () => {
+      const response = await axios({
+        method: "GET",
+        url: URL,
+      });
+      console.log(response.data);
+      this.setState({ ...this.state, ["todos"]: response.data.data });
+    };
+    getToDos();
+    console.log(this.state);
+  }
+
+  changeToDo = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+
+    this.setState({ ...this.state, ["todo"]: { ...this.state["todo"], [name]: value } });
+  };
+
+  addTask = (e) => {
+    e.preventDefault();
+    try {
+      const submitTask = async () => {
+        const response = await axios({
+          method: "POST",
+          url: URL,
+          data: {
+            name: this.state.todo.name,
+          },
+        });
+
+        this.setState({ ...this.state, ["todos"]: [...this.state.todos, [response.data.data]] });
+      };
+      submitTask();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   render() {
-    return null
+    return (
+      <>
+        <h2 className="todos-header">ToDos:</h2>
+        <section className="todos">
+          {this.state.todos &&
+            this.state.todos.map((todo, index) => {
+              return <p key={index}>{`${todo && todo.name} ${todo && todo.completed === true ? "✔" : ""}`}</p>;
+            })}
+          <Form todo={this.state.todo} changeToDo={this.changeToDo} addTask={this.addTask} />
+        </section>
+      </>
+    );
   }
 }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
-    return null
+    return (
+      <form onSubmit={(e) => this.props.addTask(e)}>
+        <input type={"text"} placeholder="To-Do Name" name="name" value={this.props.todo.name} onChange={(e) => this.props.changeToDo(e)} />
+        <button type="submit">Submit</button>
+      </form>
+    );
   }
 }
